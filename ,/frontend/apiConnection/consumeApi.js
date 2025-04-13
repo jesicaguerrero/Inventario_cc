9a5e9f24aaffcbd0ef0f2a6a832776556e237806
jessica <jeguebla00@gmail.com>
2025-04-12 20:59:56 -0500

    Actualización del backend y frontend

---
+++ ,/frontend/apiConnection/consumeApi.js
@@ -0,0 +1,12 @@
+const url = "http://localhost:5000/api/categorias"
+
+export const obtainCategories = async ()=>{
+    try{
+      const resultado = await fetch(url);
+      const categorias = await resultado.json();
+      return categorias 
+    } catch(error){
+       console.error("error"); 
+
+    }
+}
